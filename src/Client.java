d0ee270cab7e39130e7cf28dd9dd074807989309
Nic <robe1606@morris.umn.edu>
2020-09-03 13:02:47 -0500

    Pull initial progress into master

---
+++ src/Client.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Client {
+    public static void main(String agrs[]){
+
+        int[] arr = {2,1,3,7,6,4,5};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    public void quickSort(int[] intArray, int l, int r){
+        if(l < r){
+            int q = partition(intArray, l, r);
+            quickSort(intArray, l, q-1);
+            quickSort(intArray, q+1, r);
+        }
+    }
+    public int partition(int[] intArray, int l, int r){
+        int x = intArray[r];
+        int i = l - 1;
+        for(int j =l; j < r-1; j++){
+            if (intArray[j] <= x) {
+                i = i + 1;
+                //exchange(intArray, l, j);
+            }
+            //exchange(intArray, l-1, r);
+            return i + 1;
+        }
+        return 0;
+    }
+    //public void exchange(int[] intArray, int indexA, int indexB){
+     //   int temp = intArray[i];
+
+       // intArray[i] = intArray[j];
+        //intArray[j] = temp;
+    //}
+}
